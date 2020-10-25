2988522bd3212f7e077dd11a258ab72d0f2c7ff9
cesar augusto <cesar.barbosa@bandtec.com.br>
2020-10-25 11:42:16 -0300

    Atualizando heatmap

---
+++ Sites/Dashboard/charts.js
@@ -0,0 +1,60 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['18:30:27', '18:31:17', '18:32:07', '18:32:57', '18:30:57'],
+        datasets: [{
+            label: 'Grafico de uso',
+            data: [Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        tooltips: {
+            mode: 'index'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+var myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['18:30:27', '18:31:17', '18:32:07', '18:32:57', '18:30:57'],
+        datasets: [{
+            label: 'Grafico de uso',
+            data: [Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        tooltips: {
+            mode: 'index'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
---
+++ Sites/Dashboard/heatmap.js
@@ -0,0 +1,42 @@
+// ################ HEAT MAP ################# //
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+function get(id) {
+    return document.getElementById(id);
+}
+
+
+// ############### POSIÇÃO X e Y DE ONDE VAI COLOCAR O PONTO DE CALOR ##################//
+var posicaoCPU = {
+    x: 510,
+    y: 200,
+    intencidade: Math.floor(Math.random() * 51)
+}
+
+
+var posicoes = [
+    [posicaoCPU.x, posicaoCPU.y, posicaoCPU.intencidade],
+    [posicaoCPU.x - 13, posicaoCPU.y - 15, posicaoCPU.intencidade],
+    [posicaoCPU.x + 15, posicaoCPU.y + 25, posicaoCPU.intencidade],
+    [posicaoCPU.x - 13, posicaoCPU.y - 15, posicaoCPU.intencidade],
+    [posicaoCPU.x + 15, posicaoCPU.y - 14, posicaoCPU.intencidade],
+];
+var heat = simpleheat('heatMap').data(posicoes).max(18),
+    frame;
+
+function draw() {
+    console.time('draw');
+    heat.draw();
+    console.timeEnd('draw');
+    frame = null;
+}
+
+draw();
+
+// setInterval(function randomIntencity() {
+//    posicaoCPU.intencidade = Math.floor(Math.random() * 51);
+//    draw();
+//     randomIntencity();
+// }, 3000);
