311631f5a8f8016eb83299dcd6510886f62ed29c
Lucas Felipe <lucas.asantos@bandtec.com.br>
2020-11-30 18:42:24 -0300

    Deploy

--- Sites/Dashboard_new/routes/index.js
+++ Sites/Dashboard_new/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login.html', { title: 'Express' });
+  console.log('foi');
 });
 
+// app.get("/api", (req, seq))
 module.exports = router;
