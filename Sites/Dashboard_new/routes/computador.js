3e8608a8a50043f2ff60ca9ff785b578de3f7d0a
Lucas Felipe <lucas.asantos@bandtec.com.br>
2020-12-03 01:17:13 -0300

    Fazendo pesquisa por nome de máquina

--- Sites/Dashboard_new/routes/computador.js
+++ Sites/Dashboard_new/routes/computador.js
@@ -24,5 +24,29 @@ router.get('/recuperar/:fkEscola', function (req, res, next) {
     });
 });
 
+router.get('/recuperar_especifica/:fkEscola/:nomeComputador', function (req, res, next) {
+    console.log('Recuperando computador específico');
+    let instrucaoSql = `select * from Computador where fkEscola='${req.params.fkEscola}' and nomeComputador = '${req.params.nomeComputador}'`;
+
+    sequelize.query(instrucaoSql, {
+        model: Computador
+    }).then(resultado => {
+        Computador.findAll({
+            where: {
+              fkEscola: req.params.fkEscola,
+              nomeComputador: req.params.nomeComputador
+            }
+          }).then(resultado => {
+            res.json(resultado);
+        }).catch(erro => {
+            console.error(erro);
+            res.status(500).send(erro.message);
+        });
+    }).catch(erro => {
+        console.error(erro);
+        res.status(500).send(erro.message);
+    });
+});
+
 
 module.exports = router;
