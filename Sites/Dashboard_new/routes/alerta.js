6a7f0f66deff5973023c0950ddde66a8fc76d1c3
cesar augusto <cesar.barbosa@bandtec.com.br>
2020-12-10 22:37:54 -0300

    Fixando erro de query de alertas

--- Sites/Dashboard_new/routes/alerta.js
+++ Sites/Dashboard_new/routes/alerta.js
@@ -4,32 +4,27 @@ var sequelize = require('../models').sequelize;
 var Alerta = require('../models').Alerta;
 var Computador = require('../models').Computador;
 
-router.get('/recuperar/alertas', function (req, res, next) {
+router.get('/recuperar/alertas/:fkEscola', function (req, res, next) {
     console.log(`Recuperando Alertas de uma máquina especifica`);
     const hoje = new Date();
-    const hojeSqll = `${hoje.getFullYear()}-${hoje.getMonth() + 1}-${hoje.getDate()}`;
-    let instrucaoSql = `select * from Alerta, Computador where idComputador = fkComputador and
-    dataHora between '${hojeSqll} 00:00:00' and '${hojeSqll} 23:59:59';`;
+    const hojeSqll = `${hoje.getFullYear()} - ${hoje.getMonth() + 1} - ${hoje.getDate()}`;
+    console.log(hojeSqll);
+    let instrucaoSql = `select * from Alerta, Computador where idComputador = fkComputador and dataHora between '${hojeSqll} 00:00:00' and '${hojeSqll} 23:59:59' and fkEscola = ${req.params.fkEscola};`;
     console.log("Instrucao" + instrucaoSql);
     sequelize.query(instrucaoSql, {
-        model: Alerta
+        model: Alerta,
+        include: [{
+            model: Computador,
+            required: true,
+        }]
     }).then(resultado => {
-        Alerta.findAll({
-            include: [{
-                model: Computador,
-                required: true
-            }]
-        }).then(resultado => {
-            res.json(resultado);
-        }).catch(erro => {
-            console.error(erro);
-            res.status(500).send(erro.message);
-        });
+        res.json(resultado);
     }).catch(erro => {
         console.error(erro);
         res.status(500).send(erro.message);
     });
 });
+
 
 router.get('/recuperar_maquina_especifica/:idComputador', function (req, res, next) {
     console.log(`Recuperando Alertas`);
