65337455f24f7b4dbaa8688280a3bbb9bdee1143
Lucas Felipe <lucas.asantos@bandtec.com.br>
2020-12-12 21:33:37 -0300

    ajustando pasta

---
+++ Sites/site_institucional/app.js
@@ -0,0 +1,31 @@
+process.env.NODE_ENV = 'production';
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usuariosRouter = require('./routes/tecnico');
+var computadorRouter = require('./routes/computador');
+var usoTotalRouter = require('./routes/usoTotal');
+var alertaRouter = require('./routes/alerta');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/tecnico', usuariosRouter);
+app.use('/computador', computadorRouter);
+app.use('/usoTotal', usoTotalRouter);
+app.use('/alerta', alertaRouter);
+
+
+
+module.exports = app;
